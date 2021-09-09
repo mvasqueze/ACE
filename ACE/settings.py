acb2a4c86aa72859cc1f921cacdf8f8f2852b75d
mvasqueze <68928428+mvasqueze@users.noreply.github.com>
2021-09-08 22:03:09 -0500

    Settings conectado a MySQL

--- ACE/settings.py
+++ ACE/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'ACE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pi1',                     
+        'USER': 'root',                     
+        'PASSWORD': '1234',
+        "HOST": "127.0.0.1",
+        "PORT": 3306, 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -121,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
